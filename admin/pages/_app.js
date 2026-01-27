b48eba33e021621a09b75a49c428f401a15e5532
MuhammadOusman <ousmansohail786@gmail.com>
2026-01-28 03:11:22 +0500

    admin side

--- admin/pages/_app.js
+++ admin/pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+
+// No-op change to trigger Next dev recompile (no functional change)
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
